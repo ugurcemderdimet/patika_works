bbc1f2069198ace695018735a07b3d86a4d59972
ugurderdimet <61950938+ugurcemderdimet@users.noreply.github.com>
2021-05-15 15:38:21 +0300

    update variables

--- clockDateWork/js/clock.js
+++ clockDateWork/js/clock.js
@@ -1,33 +1,33 @@
 let screenName = prompt("Lütfen İsminizi Giriniz : ");
 document.getElementById("name").innerHTML = screenName;
 function showTime() {
-  var gun = new Date();
-  var saat = gun.getHours();
-  var dakika = gun.getMinutes();
-  var saniye = gun.getSeconds();
-  var session = gun.getDay();
+  var date = new Date();
+  var clock = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  var day = date.getDay();
 
-  if (session == 1) {
-    session = "Pazartesi";
-  } else if (session == 2) {
-    session = "Salı";
-  } else if (session == 3) {
-    session = "Çarşamba";
-  } else if (session == 4) {
-    session = "Perşembe";
-  } else if (session == 5) {
-    session = "Cuma";
-  } else if (session == 6) {
-    session = "Cumartesi";
-  } else if (session == 7) {
-    session = "Pazar";
+  if (day == 1) {
+    day = "Pazartesi";
+  } else if (day == 2) {
+    day = "Salı";
+  } else if (day == 3) {
+    day = "Çarşamba";
+  } else if (day == 4) {
+    day = "Perşembe";
+  } else if (day == 5) {
+    day = "Cuma";
+  } else if (day == 6) {
+    day = "Cumartesi";
+  } else if (day == 7) {
+    day = "Pazar";
   }
 
-  saat = saat < 10 ? "0" + saat : saat;
-  dakika = dakika < 10 ? "0" + dakika : dakika;
-  saniye = saniye < 10 ? "0" + saniye : saniye;
+  clock = clock < 10 ? "0" + clock : clock;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
 
-  var time = saat + ":" + dakika + ":" + saniye + " " + session;
+  var time = clock + ":" + minute + ":" + second + " " + day;
   document.getElementById("myClock").innerText = time;
   document.getElementById("myClock").textContent = time;
 
